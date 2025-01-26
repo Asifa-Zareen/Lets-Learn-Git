c80a6a580def4b83a353e50850b7cad532ac1f0a
Asifa Zareen <as572627@dal.ca>
2025-01-25 22:52:09 -0400

    initial commit

---
+++ sum.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {result}")
